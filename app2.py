99e42a0acf2af382d8f1bcf8c7739fc19db7f78d
Premgrohit45 <premgolhar0429@gmail.com>
2025-05-24 12:35:40 +0530

    Add files via upload

--- app2.py
+++ app2.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request
 import requests
+from waitress import serve
 
 app = Flask(__name__)
 
@@ -88,5 +89,6 @@ def index():
 
 
 if __name__ == '__main__':
-    app.debug = True
-    app.run()
+    # app.debug = True
+    # app.run()
+    serve(app, host="0.0.0.0", port=5000)
